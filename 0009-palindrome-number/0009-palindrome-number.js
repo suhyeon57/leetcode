511f6993c419f2396e1212f50cb19d8fe4fbd68b
SUHYEON <104812227+suhyeon57@users.noreply.github.com>
2026-05-06 16:29:56 +0900

    Time: 11 ms (25.86%), Space: 65.2 MB (21.36%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -4,13 +4,13 @@
  */
 
  //number을 string으로 변경 후 뒤집어서 비교
- 
+
 var isPalindrome = function(x) {
-    console.log(typeof(x));
+    //console.log(typeof(x));
     let stX = x.toString();
-    console.log(typeof(stX));
+    //console.log(typeof(stX));
     let udStX = stX.split("").reverse().join("");
-    console.log(udStX);
+    //console.log(udStX);
     if(x < 0){
         return false;
     }
